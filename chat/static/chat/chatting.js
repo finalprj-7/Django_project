c713f1fa26634a2298b3406a75201126aaa07722
장현지 <hy9458@duksung.ac.kr>
2023-12-02 13:49:02 +0900

    [1202] chat username 표시

--- chat/static/chat/chatting.js
+++ chat/static/chat/chatting.js
@@ -1,6 +1,13 @@
         const roomName = JSON.parse(document.getElementById('room-name').textContent);
         const chatLog = document.querySelector('#chat-log');
-        const username = "{{ self.user.username }}";  // 현재 사용자의 username
+        const username = JSON.parse(document.getElementById('user_name').textContent);  // 현재 사용자의 username
+
+        if (chatLog.childNodes.length<=1) {
+            const emptyText = document.createElement('h3')
+            emptyText.id = 'emptyText'
+            emptyText.className ='emptyText'
+            chatLog.appendChild(emptyText)
+        }
 
         const chatSocket = new WebSocket(
             'ws://'
@@ -14,24 +21,39 @@
         chatSocket.onmessage = function(e) {
             const data = JSON.parse(e.data);
             const message = data.message;
-            const username = data.username;
+            const userId = data["user_id"];
+            const userName = data["user_name"];
+            const messageElement = document.createElement('div')
+
+
+
+            const loggedInUserId = JSON.parse(document.getElementById("user_id").textContent)
+            const loggedInUserName = JSON.parse(document.getElementById("user_name").textContent)
 
             const timestamp = new Date(data.timestamp);
 
-            // console.log("Timestamp from server:", data.timestamp);  // 확인용 로그
-            //
-            // if (!isNaN(timestamp.getTime())) {
-            // }
 
-            // 받은 메시지를 chat-log에 추가 (스타일 정보를 적용)
-       //     chatLog.innerHTML += `<div class="chat-message">${message}</div>`;
-            chatLog.innerHTML += `<div class="d-flex flex-row justify-content-start"><p class="small p-2 ms-3 mb-3 rounded-3" style="background-color: #f5f6f7;">
-                 <strong>${username}: </strong> > ${message}</p>
-            <!-- <span class="text-muted">${timestamp.toLocaleString()}</span>  -->
-<!--                 <span class="text-muted">{{chatmessage.timestamp}}</span> -->
+
+            // 메시지가 접속한 유저가 보낸 메시지이면
+            if (userId === loggedInUserId) {
+                messageElement.innerHTML = `<div id="send_message" class="d-flex flex-row justify-content-end" ><p class="small p-2 ms-3 mb-3 rounded-3" style="background-color: #f5f6f7;">
+                  ${message} </p>
                  </div>`;
-            //message 구조
+                messageElement.classList.add('message', 'sender')
+            }
 
+            // 메시지가 다른 유저가 보낸 것이면...
+            else {
+                messageElement.innerHTML = `<div id="receive_message" class="d-flex flex-row justify-content-start" ><p class="small p-2 ms-3 mb-3 rounded-3" style="background-color: #f5f6f7;">
+                 <strong>${userName}> </strong> ${message} </p>
+                 </div>`;
+                messageElement.classList.add('message', 'receiver')
+            }
+
+            chatLog.appendChild(messageElement)
+
+
+            //메시지 전송 시 스크롤 내리기
                 var element = document.getElementById("chatroom_box");
                 element.scrollIntoView(false);
         };
@@ -50,8 +72,9 @@
         document.querySelector('#chat-message-submit').onclick = function(e) {
             const messageInputDom = document.querySelector('#chat-message-input');
             const message = messageInputDom.value;
+
             chatSocket.send(JSON.stringify({
-                'message': message, 'username': username,
+                'message': message,
             }));
             messageInputDom.value = '';
         };
--- chat/static/chat/scroll_to_bottom.js
+++ chat/static/chat/scroll_to_bottom.js
@@ -6,11 +6,6 @@ window.onload =function() {
 
 }
 
-  function autoResize() {
-    const input = document.getElementById('chat-message-input');
-    input.style.height = 'auto'; // Reset the height to auto to calculate the actual height needed
-    input.style.height = (input.scrollHeight) + 'px'; // Set the height to the calculated height
-  }
 
   function calcTextareaHeight(e) {
     e.style.height = 'auto'
@@ -18,50 +13,9 @@ window.onload =function() {
 }
 
 
-//
-// const scrollToBottom = () => {
-//     window.scrollTo(0, document.body.scrollHeight);
-// };
-//
-// // 페이지가 로드될 때 스크롤을 하단으로 이동
-// window.onload = scrollToBottom;
-//
-// setTimeout(function() {
-//                 window.scrollTo(0, document.body.scrollHeight);
-//             }, 500);
-//
-// // 일정 시간 간격으로 페이지를 스크롤
-// setInterval(() => {
-//     scrollToBottom();
-// }, 3000); // 예: 3초마다 스크롤
 
 
 
 
 
-//
-//
-// window.onload = function() {
-//             window.scrollTo(0, document.body.scrollHeight);
-//         };
-//
-// setTimeout(function() {
-//                 window.scrollTo(0, document.body.scrollHeight);
-//             }, 500);
-//
-//
-// // const scrollToBottom = () => {
-// //     window.scrollTo(0, document.body.scrollHeight);
-// // };
-//
-// // 페이지가 로드될 때 스크롤을 하단으로 이동
-// //window.onload = scrollToBottom;
-//
-//
-// function scrollToBottoms() {
-//     let objDiv = document.getElementsByClassName("chat-log-container");
-//     objDiv.scrollTop = objDiv.scrollHeight;
-// }
-//
-// // Add this below the function to trigger the scroll on load.
-// scrollToBottoms();
+
